35c0646fbd6c518e144b650c8b5bc539335a300c
Mark Layman <Mark.l.layman@gmail.com>
2014-10-05 21:42:00 -0500

    Add Customer and Employee Unit tests

---
+++ Chinook.Tests/Controllers/Api/CustomerApiControllerTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chinook.Web.UI.Tests.Controllers.Api
+{
+    [TestClass()]
+    public class CustomerApiControllerTests
+    {
+        [TestMethod()]
+        public void CustomerApiControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataPageableSubSetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataByCustomerIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataBySupportRepIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataBySupportRepIdPageableSubSetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Chinook.Tests/Controllers/Api/EmployeeApiControllerTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chinook.Web.UI.Tests.Controllers.Api
+{
+    [TestClass()]
+    public class EmployeeApiControllerTests
+    {
+        [TestMethod()]
+        public void EmployeeApiControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataPageableTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataByEmployeeIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataByReportsToTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataByReportsToPageableTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
