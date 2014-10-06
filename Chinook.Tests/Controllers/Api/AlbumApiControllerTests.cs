f8f6715ea1e219fd8005034e4f2cbf10f08b7c9f
Mark Layman <Mark.l.layman@gmail.com>
2014-10-05 21:36:23 -0500

    Add and configure AlbumApiController unit test

---
+++ Chinook.Tests/Controllers/Api/AlbumApiControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chinook.Web.UI.Controllers.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Chinook.Web.UI.Tests.Controllers.Api
+{
+    [TestClass()]
+    public class AlbumApiControllerTests
+    {
+        [TestMethod()]
+        public void AlbumApiControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAlbumsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAlbumsPagableTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataByAlbumIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataByArtistIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataByArtistIdPageableTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
