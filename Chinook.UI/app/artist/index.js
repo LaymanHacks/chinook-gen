85dec23c6e617969ad23b5add0f2f93c4dd8a550
Mark Layman <MarkLayman@cox.net>
2014-10-10 18:25:53 -0500

    Add angularjs services

---
+++ Chinook.UI/app/artist/index.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'artistIndexCtrl';
+    angular.module('app').controller(controllerId, ['common', 'artistDataService', artistList]);
+
+    function artistList(common, artistDataService) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.news = {
+            title: 'Artist List',
+            description: 'Artist List'
+        };
+       
+
+        vm.pagableResults = [];
+        vm.title = 'ArtistList';
+        vm.sortExpression = 'Name';
+        vm.currentPage = 1;
+        vm.pageSize = 20;
+
+        activate();
+
+        vm.pageChanged = function () {
+           
+            return getDataPageable(vm.sortExpression, vm.currentPage, vm.pageSize);
+        };
+
+        function activate() {
+            var promises = [getDataPageable(vm.sortExpression, vm.currentPage, vm.pageSize)];
+            common.activateController(promises, controllerId)
+                .then(function () { log('Activated Artist List View'); });
+        }
+        
+        function getDataPageable(sortExpression, page, pageSize) {
+            return artistDataService.getDataPageable(sortExpression, page, pageSize).then(function (results) {
+                
+                return  vm.pagableResults = results.data;
+              });
+        }
+    }
+})();
+
+angular.module('app').filter('range', function () {
+    return function (val, range) {
+        range = parseInt(range);
+        for (var i = 0; i < range; i++)
+            val.push(i);
+        return val;
+    };
+});
---
+++ Chinook.UI/app/services/artistDataService.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    'use strict';
+
+    var serviceId = 'artistDataService';
+    angular.module('app').service(serviceId, ['$http', artistDataService]);
+
+    function artistDataService($http) {
+        var urlBase = '/api/artists';
+
+        this.getData = function () {
+            return $http.get(urlBase + '/all');
+        };
+
+        this.updateArtist = function (artist) {
+            return $http.put(urlBase, artist);
+        };
+
+        this.insertArtist = function (artist) {
+            return $http.post(urlBase, artist);
+        };
+
+        this.deleteArtist = function (artistId) {
+            return $http.Delete(urlBase, artistId);
+        };
+
+        this.getDataPageable = function (sortExpression, page, pageSize) {
+            return $http({
+                url: urlBase,
+                method: 'GET',
+                params: {
+                    sortExpression: sortExpression || '',
+                    page: page || '',
+                    pageSize: pageSize || ''
+                }
+            });
+        };
+
+        this.getDataByArtistId = function (artistId) {
+            return $http.get(urlBase + '/' + artistId);
+        };
+
+
+    }
+})();
+
