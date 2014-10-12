1f266c8088a289a7b7c716de060c2d21678517ea
Mark Layman <Mark.l.layman@gmail.com>
2014-10-12 09:20:16 -0500

    Add Genre MVC controller

---
+++ Chinook.UI/Controllers/GenreController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chinook.Data.DbCommandProvider;
+using Chinook.Data.Repository;
+
+namespace Chinook.Web.UI.Controllers
+{
+    public class GenreController : Controller
+    {
+         private readonly IGenreRepository _dbRepository;
+
+        public GenreController(IDbGenreCommandProvider dbCommandProvider)
+        {
+            _dbRepository = new DbGenreRepository(dbCommandProvider);
+        }
+        // GET: Genre
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Genre/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Genre/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Genre/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Genre/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Genre/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Genre/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Genre/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
