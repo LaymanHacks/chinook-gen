96bffc0e878a73cf1d8f1ea6a715bb5ca187f751
Mark Layman <Mark.l.layman@gmail.com>
2014-10-23 20:43:50 -0500

    Add AngularJS Invoice Controller

---
+++ Chinook.UI/app/invoice/index.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'invoiceIndexCtrl';
+    angular.module('app').controller(controllerId, ['common', 'invoiceDataService', invoiceList]);
+
+    function invoiceList(common, invoiceDataService) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.news = {
+            title: 'Invoice List',
+            description: 'Invoice List'
+        };
+       
+        vm.pagableResults = [];
+        vm.title = 'InvoiceList';
+        vm.sortExpression = 'Name';
+        vm.currentPage = 1;
+        vm.pageSize = 10;
+        vm.deleteInvoice = deleteInvoice;
+
+        activate();
+
+        vm.pageChanged = function () {
+            return getDataPageable(vm.sortExpression, vm.currentPage, vm.pageSize);
+        };
+
+        function activate() {
+            var promises = [getDataPageable(vm.sortExpression, vm.currentPage, vm.pageSize)];
+            common.activateController(promises, controllerId)
+                .then(function () { log('Activated Invoice List View'); });
+        }
+        
+        function getDataPageable(sortExpression, page, pageSize) {
+            return invoiceDataService.getDataPageable(sortExpression, page, pageSize).then(function (results) {
+                return  vm.pagableResults = results.data;
+              });
+        }
+
+        function deleteInvoice(invoiceId) {
+            alert("test worked");
+          //  return invoiceDataService.deleteInvoice(invoiceId);
+        };
+    }
+})();
+
